38651e3f3c872578f8e64c40b0d411a894d0e0cd
Luciano Lim <lmlim@cpp.edu>
2024-01-21 16:59:37 -0800

    adds discord bot

--- src/bot.ts
+++ src/bot.ts
@@ -1,35 +1,70 @@
 import { SerialPort } from "serialport";
-// Port can be foudn in bash using `ls /dev/tty*`
+import * as dotenv from "dotenv";
+import { Client, GatewayIntentBits } from "discord.js";
+
 const PORT = "COM3";
-const MSG = "a";
 const BAUD_RATE = 9600;
 
-const serialPort = new SerialPort({
-  path: PORT,
-  baudRate: BAUD_RATE,
-  autoOpen: true,
-});
-serialPort.on("error", console.log);
+const READY_MESSAGE = "Y";
+const NOT_READY_MESSAGE = "N";
 
-async function main() {
-  // Opens port
-  await new Promise((res) => serialPort.close(res));
-  // Opens port
-  await new Promise((res) => serialPort.open(res));
+const SPIN_MESSAGE = "a";
+const NO_SPIN_MESSAGE = "b";
 
-  // Prints any serial data from arduino
+let spinsRemaining = 0;
+
+async function pause(timeInMs: number) {
+  await new Promise((res) => setTimeout(res, timeInMs));
+}
+
+function initSerialPort(): SerialPort {
+  const serialPort = new SerialPort({
+    path: PORT,
+    baudRate: BAUD_RATE,
+    autoOpen: true,
+  });
+  serialPort.on("error", console.log);
   serialPort.on("data", console.log);
+  return serialPort;
+}
+
+function initDiscordClient() {
+  dotenv.config();
+  const { BOT_TOKEN } = process.env;
+  if (BOT_TOKEN == undefined) throw new Error("MAKE SURE TO PUT A BOT_TOKEN env variable");
+
+  const client = new Client({ intents: [GatewayIntentBits.GuildMembers] });
+
+  client.on("guildMemberAdd", () => {
+    console.log("New user joined. Spins remaining now at", spinsRemaining + 1);
+    spinsRemaining += 1;
+  });
+
+  client.login(BOT_TOKEN);
+}
+
+async function initQueue(serialPort: SerialPort) {
+  let latestMessage = NOT_READY_MESSAGE;
+
+  serialPort.on("data", (chunk) => {
+    latestMessage = Buffer.from(chunk).toString();
+  });
 
   while (true) {
-    // Sends message through serial to arduino
-    serialPort.write(MSG);
-    // Pauses for 10 ms
-    await new Promise((resolve) => setTimeout(resolve, 10));
-    // Sends "b" through serial to arduino
-    serialPort.write("b");
-    // Pauses for 10 seconds -10 ms
-    await new Promise((resolve) => setTimeout(resolve, 10000 - 10));
+    if (spinsRemaining > 0 && latestMessage === READY_MESSAGE) {
+      spinsRemaining -= 1;
+      serialPort.write(SPIN_MESSAGE);
+      await pause(20);
+      serialPort.write(NO_SPIN_MESSAGE);
+    }
+    await pause(100);
   }
+}
+
+async function main() {
+  const serialPort = initSerialPort();
+  initDiscordClient();
+  initQueue(serialPort);
 }
 
 main();
--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,66 @@
+// Arduino Sketch
+
 #include <Arduino.h>
-#include <Stepper.h>
 
-// Defines the number of steps per rotation
-const int stepsPerRevolution = 2038;
-
-// Creates an instance of stepper class
-// Pins entered in sequence IN1-IN3-IN2-IN4 for proper step sequence
-Stepper myStepper = Stepper(stepsPerRevolution, 2, 3, 4, 5);
+const int stepPin = 5;
+const int dirPin = 6;
+const int enPin = 7;
+const int stepPin1 = 8;
+const int dirPin2 = 9;
+const int enPin3 = 10;
 
 void setup()
 {
-  Serial.begin(9600);
+  Serial.begin(9600); // Set the baud rate to match the Python script
+
+  pinMode(stepPin, OUTPUT);
+  pinMode(dirPin, OUTPUT);
+  pinMode(enPin, OUTPUT);
+  digitalWrite(enPin, LOW);
+
+  pinMode(stepPin1, OUTPUT);
+  pinMode(dirPin2, OUTPUT);
+  pinMode(enPin3, OUTPUT);
+  digitalWrite(enPin3, LOW);
 }
 
 void loop()
 {
   char responseChar = 'Y'; // Response, available
-  Serial.print(responseChar);
+  Serial.println(responseChar);
 
-  // Rotate CW slowly at 5 RPM
-  // myStepper.setSpeed(7);
-  // myStepper.step(stepsPerRevolution);
-  // delay(1000);
+  if (Serial.available() > 0)
+  {
+    // Read a character from the serial port
+    char receivedChar = Serial.read();
+
+    if (receivedChar == 'a')
+    {
+      char responseChar = 'N'; // Response, not available, since the motors are in action
+      Serial.println(responseChar);
+
+      // Motor Operation for the first motor
+      digitalWrite(dirPin, HIGH); // Set direction for the first motor
+      for (int x = 0; x < 800; x++)
+      {
+        digitalWrite(stepPin, HIGH);
+        delayMicroseconds(1050);
+        digitalWrite(stepPin, LOW);
+        delayMicroseconds(1800);
+      }
+
+      // Motor Operation for the second motor
+      digitalWrite(dirPin2, HIGH); // Set direction for the second motor
+      for (int x = 0; x < 800; x++)
+      {
+        digitalWrite(stepPin1, HIGH);
+        delayMicroseconds(1050);
+        digitalWrite(stepPin1, LOW);
+        delayMicroseconds(1800);
+      }
+
+      // Wait for a short delay
+      delay(1000);
+    }
+  }
 }
